c69c47182f8cded1f6c799a7bfa67f0540ddb090
M.A Monaem Khan <khanmonaem07@gmail.com>
2025-11-30 17:36:11 +0600

    Auth Guard

--- day-1-practice/src/app/app.routes.ts
+++ day-1-practice/src/app/app.routes.ts
@@ -1,14 +1,22 @@
 import { Routes } from '@angular/router';
+import { Authgurd } from './services/shared/authgurd/authgurd';
 
 export const routes: Routes = [
   {
     path:"",
     pathMatch:'full',
     loadComponent: ()=> {return import('./component/home/home').then((m)=>m.Home)},
+    canActivate:[Authgurd]
   },
   {
     path:"about",
     pathMatch:'full',
     loadComponent: ()=>{return import('./component/about/about').then((m)=>m.About)},
+    canActivate:[Authgurd]
+  },
+  {
+    path:"error",
+    pathMatch:"full",
+    loadComponent: ()=>{return import('./component/error/error').then((m)=>m.Error)},
   }
 ];
---
+++ day-1-practice/src/app/services/shared/authgurd/authgurd.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthgurdService } from './authgurd.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Authgurd implements CanActivate{
+  constructor(private AuthgurdService : AuthgurdService,private router: Router){}
+
+  canActivate(): boolean {
+if (!this.AuthgurdService.isAuthorized()) {
+      this.router.navigate(['/error']);
+      return false;
+    }
+    return true;
+  }
+
+}
