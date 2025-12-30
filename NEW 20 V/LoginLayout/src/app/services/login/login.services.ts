0fad122a6267d95e990ec39c908ebfadb9c26c81
M.A Monaem Khan <khanmonaem07@gmail.com>
2025-12-30 10:55:26 +0600

    ok

--- NEW 20 V/LoginLayout/src/app/services/login/login.services.ts
+++ NEW 20 V/LoginLayout/src/app/services/login/login.services.ts
@@ -2,16 +2,21 @@ import { Injectable } from '@angular/core';
 import { SessionService } from '../shared/session/session.service';
 import { Guid } from '../../Class/Guid';
 import { Session } from '../../Class/session';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginServices {
   constructor(
-    private sessionService : SessionService
+    private sessionService : SessionService,
+    private http : HttpClient
   ){}
 
   async doLogin(userId:string,password:string,isRemember:boolean):Promise<string>{
+    this.http.get("https://jsonplaceholder.typicode.com/todos/1").subscribe(data=>{
+      console.log(data);
+    });
     if(userId !== "admin"){
       return "User Not Found";
     }else if(password !== "1"){
